31c67bedd60fdf0b1f9455d54db9ced63c04192c
shubhindia <shubhindia123@gmail.com>
2023-10-12 11:07:20 +0530

    initial RED metrics
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- golang/main.go
+++ golang/main.go
@@ -6,10 +6,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+// Rate metric
+var requestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "http_requests_total",
+	Help: "Total number of HTTP requests",
+}, []string{"method"})
+
+// Errors metric
+var errorsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "http_errors_total",
+	Help: "Total number of HTTP errors",
+}, []string{"method"})
+
+// Duration metric
+var requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name:    "http_request_duration_seconds",
+	Help:    "HTTP request duration in seconds",
+	Buckets: prometheus.ExponentialBuckets(0.05, 1.5, 10),
+}, []string{"method"})
 
 var pongCounter = prometheus.NewCounter(
 	prometheus.CounterOpts{
@@ -23,6 +43,14 @@ var pongCounter = prometheus.NewCounter(
 
 func pong(w http.ResponseWriter, r *http.Request) {
 	pongCounter.Inc()
+	startTime := time.Now()
+	time.Sleep(5 * time.Second)
+	defer func() {
+		duration := time.Since(startTime).Seconds()
+		method := r.Method
+		requestsTotal.WithLabelValues(method).Inc()
+		requestDuration.WithLabelValues(method).Observe(duration)
+	}()
 	w.Header().Set("Content-Type", "application/json")
 	hostname, _ := os.Hostname()
 	resp := map[string]string{
@@ -50,7 +78,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	httpPort := 8080
-	prometheus.MustRegister(pongCounter)
+	prometheus.MustRegister(pongCounter, requestsTotal, errorsTotal, requestDuration)
 	http.HandleFunc("/healthz", homePage)
 	http.HandleFunc("/ping", pong)
 	http.Handle("/metrics", promhttp.Handler())
